e4cdcc458f73113e0702a8d8f1c8ae6417eeca89
Danos <garciadanos@gmail.com>
2021-08-30 16:17:55 -0300

    working on NotFound component

--- Frontend/portfolio/src/Landing.js
+++ Frontend/portfolio/src/Landing.js
@@ -1,4 +1,5 @@
 import "./Css/Landing.css";
+import NotFound from "./Components/NotFound";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import Content from "./Components/Content";
 import NavigationBar from "./Components/Navbar";
@@ -6,9 +7,12 @@ export default function Landing() {
   return (
     <BrowserRouter>
       <Switch>
-        <NavigationBar />
-        <Content />
-        <h1> Hola mundo</h1>
+        <Route exact path="/">
+          <NavigationBar />
+          <Content />
+          <h1> Hola mundo</h1>
+        </Route>
+        <NotFound />
       </Switch>
     </BrowserRouter>
   );
