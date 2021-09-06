7955d2b9cdcff823fa79b89f16a846ea8fbc37fb
Danos <garciadanos@gmail.com>
2021-09-06 15:59:00 -0300

    Created Proyects component

---
+++ Frontend/src/Components/Proyects.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Css/Proyects.css";
+export default function Proyects() {
+  return (
+    <div id="proyects-wrapper">
+      <div id="proyect-left-container">
+        <h1>Brief resume of some of my proyects</h1>
+      </div>
+      <div id="proyect-right-container">
+        <h1>Brief resume of some of my proyects</h1>
+      </div>
+    </div>
+  );
+}
