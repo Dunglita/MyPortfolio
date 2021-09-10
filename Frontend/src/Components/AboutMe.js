5ca01c284b8526d1f48070d460e96acec738c37b
Danos <garciadanos@gmail.com>
2021-09-10 01:13:53 -0300

    Description and Button remork

--- Frontend/src/Components/AboutMe.js
+++ Frontend/src/Components/AboutMe.js
@@ -1,31 +1,39 @@
 import React from "react";
 import "../Css/AboutMe.css";
 import me from "../Assets/Images/me.png";
+import Cv from "../Assets/Docs/CVES.pdf";
 export default function AboutMe() {
   return (
     <>
-      <div className="about-me">
-        <img id="me-img" src={me} />
+      <div className="about-me" id="about-me">
+        <img id="me-img" src={me} alt="Daniel Garcia Images" />
         <div id="right-content">
-          <h1> About me</h1>
+          <h1>About me</h1>
           <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor si voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur
+            Hi, my name is Daniel Garcia, Dani for short, I'm a Fullstack
+            Developer bassed in Mendoza, Argentina.
+            <br />
+            <br />
+            Actually I'm involved in the area of Web Developping using React and
+            NodeJS, having special interest in Frontend and UX/UI desing. Also
+            at the moment I'm learning Angular and doing a Java course.
+            <br />
+            <br />
+            Currently I'm in the search of an oportunity to join a team and
+            start working to expand my horizons and gain professional
+            experience.
           </p>
-          <div id="btn-group">
-            <button id="btn-cv" type="button" className="btn btn-warning">
-              Descargar CV
-            </button>
-            <button type="button" className="btn btn-outline-warning">
-              Portfolio
-            </button>
-          </div>
+          <h1>Knowledge</h1>
+          <a
+            id="btn-cv"
+            type="button"
+            className="btn btn-outline"
+            href={Cv}
+            rel="noreferrer"
+            target="_blank"
+          >
+            View Resume
+          </a>
         </div>
       </div>
     </>
