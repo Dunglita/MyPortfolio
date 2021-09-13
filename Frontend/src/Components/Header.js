48e5745652c28a093dd77a423f8079668391685b
Danos <garciadanos@gmail.com>
2021-09-13 06:57:55 -0300

    Added header's dot ids

--- Frontend/src/Components/Header.js
+++ Frontend/src/Components/Header.js
@@ -8,6 +8,8 @@ export default function Header() {
           <h4>Junior</h4>
           <h3>Web Developer</h3>
           <p>DANI</p>
+          <span id="header-dot1" className="dot-big"></span>
+          <span id="header-dot2" className="dot-small"></span>
         </div>
       </div>
     </>
