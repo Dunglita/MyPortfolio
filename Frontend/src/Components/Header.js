02a14bf3253960cccb238010e87dd736fa436d7c
Danos <garciadanos@gmail.com>
2021-09-16 18:36:04 -0300

    Added waves bakcground to header

--- Frontend/src/Components/Header.js
+++ Frontend/src/Components/Header.js
@@ -5,9 +5,11 @@ export default function Header() {
     <>
       <div className="top">
         <div className="header">
-          <h4>Junior</h4>
-          <h3>Web Developer</h3>
-          <p>DANI</p>
+          <div className="header-content">
+            <h4>Junior</h4>
+            <h3>Web Developer</h3>
+            <p>DANI</p>
+          </div>
           <span id="header-dot1" className="dot-big"></span>
           <span id="header-dot2" className="dot-small"></span>
         </div>
