606177587d0fe9c65afb0246f04e7a1d0c8b8e3b
Dani <garciadanos@gmail.com>
2021-11-19 05:59:03 -0300

    added tool icons

--- Frontend/src/Components/Contact.js
+++ Frontend/src/Components/Contact.js
@@ -1,127 +1,107 @@
 import React, { useState } from "react";
 import "../Css/Contact.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
 export default function Contact() {
-  const [clipboard, setClipboard] = useState("true");
-  const handleClipboard = () => {
-    setClipboard(!clipboard);
-  };
-  const handleCheckCliplboard = () => {
-    setClipboard("true");
+  /*Switches between clipboard and check icons */
+  const [clipboard, setClipboard] = useState(true);
+  const handleClipboard = (newClipboard) => {
+    setClipboard(newClipboard);
   };
   function coptyToClipbaord(e, info) {
     e.preventDefault();
-    function clippy() {
-      navigator.clipboard.writeText(info);
-    }
-    clippy();
-    handleClipboard();
+    navigator.clipboard.writeText(info);
+    handleClipboard(false);
   }
+  const [cardHoverMail, setCardHoverMail] = useState(false);
+  const handleCardHoverMail = () => {
+    setCardHoverMail(!cardHoverMail);
+  };
+  const [cardHoverPhone, setCardHoverPhone] = useState(false);
+  const handleCardHoverPhone = () => {
+    setCardHoverPhone(!cardHoverPhone);
+  };
   return (
     <>
-      <div id="contact-me" className="contact-wrapper">
-        <Container fluid id="contact-container">
-          <Container fluid id="contact-container1">
-            <Row>
-              <Col id="contact-heading">
-                <h1>Contact me</h1>
-                <h5>I'AM AVAILABLE FOR WORK, FEEL FREE TO GET IN TOUCH.</h5>
-              </Col>
-            </Row>
-          </Container>
-          <Container fluid id="contact-container2">
-            <Row id="contact-icon">
-              <Col>
-                <i class="bi bi-envelope"></i>
-                <h5>Email</h5>
-                <div
-                  id="inli"
-                  onMouseLeave={() => {
-                    handleCheckCliplboard();
-                  }}
-                >
-                  <h3 id="clipboard-container">garciadanos@gmail.com</h3>
-                  <a
-                    title="Copy email to ClipBoard"
-                    href="Notfound.html"
-                    id="clipboard-btn2"
-                    onClick={(e) =>
-                      coptyToClipbaord(e, "garciadanos@gmail.com")
-                    }
-                  >
-                    <h3 id="clipboard-container">
-                      <i
-                        className={
-                          clipboard ? "bi bi-clipboard" : "bi bi-check-lg"
-                        }
-                      ></i>
-                    </h3>
-                  </a>
-                </div>
-              </Col>
-              <Col>
-                <i class="bi bi-phone"></i>
-                <h5>Phone</h5>
-                <div
-                  id="inli"
-                  onMouseLeave={() => {
-                    handleCheckCliplboard();
-                  }}
-                >
-                  <h3>+5492616681830</h3>
-                  <a
-                    title="Copy phone to ClipBoard"
-                    href="Notfound.html"
-                    id="clipboard-btn1"
-                    onClick={(e) => coptyToClipbaord(e, "+5492616681830")}
-                  >
-                    <h3 id="clipboard-container">
-                      <i
-                        className={
-                          clipboard ? "bi bi-clipboard" : "bi bi-check-lg"
-                        }
-                      ></i>
-                    </h3>
-                  </a>
-                </div>
-              </Col>
-              <Col>
-                <i class="bi bi-geo-alt"></i>
-                <h5>Adress</h5>
-                <div
-                  id="inli"
-                  onMouseLeave={() => {
-                    handleCheckCliplboard();
-                  }}
-                >
-                  <h3>Mendoza, Argentina</h3>
-                  <a
-                    title="Copy adress to ClipBoard"
-                    href="Notfound.html"
-                    id="clipboard-btn1"
-                    onClick={(e) =>
-                      coptyToClipbaord(
-                        e,
-                        "https://www.google.com/maps/place/Mendoza,+Argentina/"
-                      )
-                    }
-                  >
-                    <h3 id="clipboard-container">
-                      <i
-                        className={
-                          clipboard ? "bi bi-clipboard" : "bi bi-check-lg"
-                        }
-                      ></i>
-                    </h3>
-                  </a>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </Container>
+      <div className="contact_wrapper" id="contact">
+        <div className="contact_title">
+          <h1>Contact</h1>
+          <h5>I'AM AVAILABLE FOR WORK, FEEL FREE TO GET IN TOUCH.</h5>
+        </div>
+        <div className="card_container">
+          <div
+            className="contact_card"
+            onMouseLeave={() => {
+              handleClipboard(true);
+            }}
+          >
+            <div className={cardHoverMail ? "face1 face1_mobile" : "face1"}>
+              <div className="icon">
+                <i className="bi-envelope"></i>
+              </div>
+              <button
+                className="btn btn-outline mobile_btn"
+                onClick={() => handleCardHoverMail()}
+              >
+                <i
+                  className={
+                    cardHoverMail
+                      ? "bi bi-chevron-compact-up"
+                      : "bi bi-chevron-compact-down"
+                  }
+                ></i>
+              </button>
+            </div>
+            <div className={cardHoverMail ? "face2 face2_mobile" : "face2"}>
+              <h6>Email</h6>
+              <h5> garciadanos@gmail.com</h5>
+              <a
+                title="Copy email"
+                href="Notfound.html"
+                onClick={(e) => coptyToClipbaord(e, "garciadanos@gmail.com")}
+              >
+                <i
+                  className={clipboard ? "bi bi-clipboard" : "bi bi-check-lg"}
+                ></i>
+              </a>
+            </div>
+          </div>
+          <div
+            className="contact_card"
+            onMouseLeave={() => {
+              handleClipboard(true);
+            }}
+          >
+            <div className={cardHoverPhone ? "face1 face1_mobile" : "face1"}>
+              <div className="icon">
+                <i className="bi bi-phone"></i>
+              </div>
+              <button
+                className="btn btn-outline mobile_btn"
+                onClick={() => handleCardHoverPhone()}
+              >
+                <i
+                  className={
+                    cardHoverPhone
+                      ? "bi bi-chevron-compact-up"
+                      : "bi bi-chevron-compact-down"
+                  }
+                ></i>
+              </button>
+            </div>
+            <div className={cardHoverPhone ? "face2 face2_mobile" : "face2"}>
+              <h6>Phone</h6>
+              <h5>+5492616681830</h5>
+              <a
+                title="Copy phone"
+                href="Notfound.html"
+                onClick={(e) => coptyToClipbaord(e, "+5492616681830")}
+              >
+                <i
+                  className={clipboard ? "bi bi-clipboard" : "bi bi-check-lg"}
+                ></i>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
